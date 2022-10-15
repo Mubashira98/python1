8e9e779dde84760424e67a656f61ef5f965253b0
Mubashira98 <mubashiravm98@gmail.com>
2022-10-15 11:44:22 +0530

    python1

---
+++ main.py
@@ -0,0 +1,70 @@
+# a="This is my first python class"
+# print(a)
+
+# -----------------------------
+
+# a=5
+# b=7
+# print("addition:",a+b)
+# print("subtraction:",a-b)
+# print("multiplication:",a*b)
+# print("division:",a/b)
+
+# ----------------------
+
+# print("hello world")
+# a=["mubi","hiba","sana","sahala"]
+# a.append("ab")
+# print(a)
+# print(len(a))
+# list1=[True,False]
+# print(list1)
+# list2=["mubi",1,True]
+# print(list2)
+
+# --------------------------------
+a=["a","b","c"]
+b=("A","B",1,"C")
+c={"hello","hai",45}
+d={"name":"mubi","age":22,"course":"python"}
+print(a)
+print(b)
+print(c)
+print(d)
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
